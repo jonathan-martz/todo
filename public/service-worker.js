9e3fd111d4b45fa9d60a804a0a991d591443306d
Jonathan Martz <git@jonathan-martz.de>
2025-02-12 21:26:36 +0100

    reset service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,77 +1,13 @@
-const CACHE_NAME = 'my-cache-v1';
-const CACHE_DURATION = 24 * 60 * 60 * 1000; // 1 day in milliseconds
-
-// List of specific files to cache
-const FILES_TO_CACHE = [
-  '/',
-  '/_nuxt/'
-];
-
 self.addEventListener('install', (event) => {
-  console.log('Service Worker installing.');
-  self.skipWaiting(); // Force the waiting service worker to become the active service worker
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(FILES_TO_CACHE);
-    })
-  );
-});
-
-self.addEventListener('activate', (event) => {
-  console.log('Service Worker activating.');
-  event.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
-        cacheNames.map((cacheName) => {
-          if (cacheName !== CACHE_NAME) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
-  );
-});
-
-self.addEventListener('fetch', (event) => {
-  console.log('Fetching:', event.request.url);
-  if (FILES_TO_CACHE.includes(new URL(event.request.url).pathname)) {
-    event.respondWith(
-      caches.match(event.request).then((cachedResponse) => {
-        if (cachedResponse) {
-          const dateHeader = cachedResponse.headers.get('date');
-          if (dateHeader) {
-            const dateCached = new Date(dateHeader);
-            const now = new Date();
-            if (now - dateCached > CACHE_DURATION) {
-              return fetchAndCache(event.request);
-            }
-          }
-          return cachedResponse;
-        }
-        return fetchAndCache(event.request);
-      })
-    );
-  } else {
-    event.respondWith(fetch(event.request));
-  }
-});
-
-function fetchAndCache(request) {
-  return fetch(request).then((response) => {
-    if (!response || response.status !== 200 || response.type !== 'basic') {
-      return response;
-    }
-    const responseToCache = response.clone();
-    caches.open(CACHE_NAME).then((cache) => {
-      const headers = new Headers(responseToCache.headers);
-      headers.append('date', new Date().toUTCString());
-      const responseWithDate = new Response(responseToCache.body, {
-        status: responseToCache.status,
-        statusText: responseToCache.statusText,
-        headers: headers,
-      });
-      cache.put(request, responseWithDate);
-    });
-    return response;
+    console.log('Service Worker installing.');
+    // Perform install steps
   });
-}
+  
+  self.addEventListener('activate', (event) => {
+    console.log('Service Worker activating.');
+  });
+  
+  self.addEventListener('fetch', (event) => {
+    console.log('Fetching:', event.request.url);
+    // You can add custom fetch handling here
+  });
