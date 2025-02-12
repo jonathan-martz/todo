327b9fa0d7940468cb34c77714987eea9288e847
Jonathan Martz <git@jonathan-martz.de>
2025-02-12 21:40:16 +0100

    favicon use jonazhan

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -11,7 +11,7 @@ export default defineNuxtConfig({
     app: {
         head: {
             link: [
-                { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+                { rel: 'icon', type: 'image/x-icon', href: 'https://cdn.jmse.cloud/avatar-2024-modified.png' }
             ],
             title: 'Todos.Martz.cloud'
         }
